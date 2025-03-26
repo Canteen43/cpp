769864a0be0079f7660279a8684bb8996119580a
Karl's WSL <karl.weihmann@gmail.com>
2025-03-26 08:43:47 +0100

    6 01: refined test

--- 6/ex01/main.cpp
+++ 6/ex01/main.cpp
@@ -6,7 +6,9 @@ int main()
 	fortyTwoNeukoelln.latitude = 52.484453339301204;
 	fortyTwoNeukoelln.longitude = 13.449772146694569;
 	fortyTwoNeukoelln.motto = "Whenever something is done badly, we will claim it is on purpose to facilitate your personal growth :)\n";
+	std::cout << &fortyTwoNeukoelln << "\n";
 	uintptr_t foo = Serializer::serialize(&fortyTwoNeukoelln);
+	std::cout << foo << "\n";
 	Data *bar = Serializer::deserialize(foo);
 	std::cout << bar->latitude << ", " << bar->longitude << ": " << bar->motto;
 }
