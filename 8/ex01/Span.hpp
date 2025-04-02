cdcc60f8935014b287343b2125cbd4a227f6b19a
Karl's WSL <karl.weihmann@gmail.com>
2025-04-03 00:32:54 +0200

    8 01: almost done, fix for addRange needed

--- 8/ex01/Span.hpp
+++ 8/ex01/Span.hpp
@@ -14,6 +14,13 @@ class Span
 		Span& operator=(const Span& other);
 		~Span();
 		void addNumber(int newNumber);
+		template <typename iterator>
+		void addRange(unsigned int rangeSize, iterator begin, iterator end)
+		{
+			if (bucket.size() + rangeSize > capacity)
+				throw std::runtime_error("Range would exceed capacity.");
+			bucket.insert(begin, end);
+		}
 		int shortestSpan();
 		int longestSpan();
 };
