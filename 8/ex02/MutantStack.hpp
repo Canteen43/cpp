cfbfe98f19ca47cba3a10e0982ac5a4b3f5dd298
Canteen43 <karl.weihmann@gmail.com>
2025-04-05 18:16:25 +0200

    8 02: added some ideas

--- 8/ex02/MutantStack.hpp
+++ 8/ex02/MutantStack.hpp
@@ -5,4 +5,12 @@ class MutantStack : public std::stack
 
 };
 
+/*Ideas:
+The stack has a member variable container.
+Its functions are all implemented using the functions of the base container.
+The container is only 'protected' so that MutantStack can still access it.
+Questions:
+1) Do I really need rend, rbegin and all the const stuff?
+2) What is the syntax for writing this kind of stuff? */
+
 #include "MutantStack.tpp"
